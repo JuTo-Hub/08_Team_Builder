35e4c49afc3664f548a14cabe5defd5f9bfd1122
JuTo_Hub <JustinTorres1@Outlook.com>
2020-10-19 18:37:50 -0500

    Finished Engineer & Intern unit tests

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+'use strict';
+const Employee = require("./Employee");
+
+class Manager extends Employee{
+    constructor(name, id, email, github){
+        super(name, id, email)
+    }
+    
+    getRole(){
+        return "Manager";
+    }
+
+}
+module.exports = Manager;
